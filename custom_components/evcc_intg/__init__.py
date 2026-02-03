165a15edf5260879164d86f26933ceef98a779d1
marq24 <marq24@emac.de>
2026-02-03 09:10:55 +0100

    make sure that all unique_id's are lower case

--- custom_components/evcc_intg/__init__.py
+++ custom_components/evcc_intg/__init__.py
@@ -910,7 +910,7 @@ class EvccBaseEntity(Entity):
     @property
     def unique_id(self):
         """Return a unique ID to use for this entity."""
-        return f"{DOMAIN}.{self.entity_id.split('.')[1]}"
+        return f"{DOMAIN}.{self.entity_id.split('.')[1]}".lower()
 
     async def async_added_to_hass(self):
         """Connect to dispatcher listening for entity data notifications."""
